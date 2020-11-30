2a3e319e601c69ad3d6a0d96edaa0f2b27a661d3
John William Becker Jr <Johnwilliambecker@gmail.com>
2020-11-30 15:04:31 -0500

    FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>fuck you, nerd dork ass whatever, working sucks, be cool</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Schedule</h4>
+        <div className="answer">
+          <p>fuck you, nerd dork ass whatever, working sucks, be cool</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment?</h4>
+        <div className="answer">
+          <p>fuck you, nerd dork ass whatever, working sucks, be cool</p>
+        </div>
+      </div>
+      <div className="question">
+        <h2>Who cares?</h2>
+        <div className="answer">
+          <p>fuck you, nerd dork ass whatever, working sucks, be cool</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
