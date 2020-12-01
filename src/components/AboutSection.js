17b76e22575de7b2df2b6e3d774b9db1c82d4a0f
John William Becker Jr <Johnwilliambecker@gmail.com>
2020-12-01 14:43:46 -0500

    "Updated Pictures"

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home1 from "../img/home1.png";
+import England from "../img/England.jpeg";
 //import styled
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
@@ -10,24 +10,22 @@ const AboutSection = () => {
       <Description>
         <div className="title">
           <Hide>
-            <h2>We work to make</h2>
+            <h2>Suck my shit</h2>
           </Hide>
           <Hide>
             <h2>
-              Your <span>dreams</span> come{" "}
+              balls <span>boobs</span> heck{" "}
             </h2>
           </Hide>
           <Hide>
             <h2>true.</h2>
           </Hide>
         </div>
-        <p>
-          Contact us for anything fuck you kiss my ass ill figure it out later.
-        </p>
-        <button>Contact Us</button>
+        <p>fuck you kiss my ass ill figure it out later.</p>
+        <button>Balls</button>
       </Description>
       <Image>
-        <img src={Home1} alt="Guy with fuck you" />
+        <img src={England} alt="Guy with fuck you" />
       </Image>
     </About>
   );
